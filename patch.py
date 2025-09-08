29719d63e2186b9e878e78792c809bf5099222d5
BuildTools <unconfigured@null.spigotmc.org>
2025-09-08 21:30:24 +0900

    wrong patch

--- patch.py
+++ patch.py
@@ -4,7 +4,7 @@ for file in glob.glob("assets/*.js"):
     src = open(file, "r", encoding="utf-8").read()
     before = src
     original = src
-    src = src.replace('"/generator/image"', '"//pixai"')
+    src = src.replace('"/generator/image"', '"/pixai"')
     if src != original:
         original = src
         print("[1] patched", file)
@@ -16,20 +16,24 @@ for file in glob.glob("assets/*.js"):
     if src != original:
         original = src
         print("[3] patched", file)
-    if 'title:"common:home.label"' in src:
-        index1 = src.index('title:"common:home.label"')
-        index2 = src[index1:].index('link:')
-        if src[index1+index2:index1+index2+8] == 'link:"/"':
-            src = src[:index1+index2] + 'link:"/pixai"'+ src[index1+index2+8:]
+    src = src.replace('.includes("pixai.art")', '.includes(".")')
     if src != original:
         original = src
         print("[4] patched", file)
+    #if 'title:"common:home.label"' in src:
+    #    index1 = src.index('title:"common:home.label"')
+    #    index2 = src[index1:].index('link:')
+    #    if src[index1+index2:index1+index2+8] == 'link:"/"':
+    #        src = src[:index1+index2] + 'link:"/pixai"'+ src[index1+index2+8:]
+    #if src != original:
+    #    original = src
+    #    print("[5] patched", file)
     if 'function setLocaleToUrl(' in src:
         index1 = src.index('function setLocaleToUrl(')
         index2 = src[index1:].index('){')
         src = src[:index1+index2+2] + 'if(true){return}'+ src[index1+index2+2:]
     if src != original:
         original = src
-        print("[5] patched", file)
+        print("[6] patched", file)
     if before != src:
         open(file, "w", encoding="utf-8").write(src)
